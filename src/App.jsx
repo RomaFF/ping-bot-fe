274974515ca1837f47d871c5913d04c235e6eea5
RomaFF <roma21479@gmail.com>
2025-08-10 01:21:22 +0300

    Ping functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,57 @@ function App() {
     console.log(tg.initData)
 
     useEffect(() => {
-        // tg.on('close', onClose)
         tg.ready();
     }, [])
 
-    const onClose = () => {
-        setData(tg.initData)
+    const onPing = async () => {
+        const user = tg.initDataUnsafe?.user;
+
+        if (!user) {
+            alert('User not found');
+            return;
+        }
+
+        const payload = {
+            chatId: user.id,
+            role: 'CLIENT',
+        };
+
+        setData(`Data sending...`);
+
+        try {
+            const response = await fetch('https://c6f39e95ba82.ngrok-free.app/ping', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload),
+            });
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const result = await response.json();
+            setData(`Sending success: ${JSON.stringify(result)}`);
+        } catch (err) {
+            console.error(err);
+            setData('Something went wrong');
+        }
 
         setTimeout(() => {
-            tg.close()
-        }, [5000])
-    }
+            tg.close();
+        }, 5000);
+    };
 
-  return (
+    return (
       <div className="container">
           <div>{data}</div>
-          <button className="button" onClick={onClose}>
+          <button className="button" onClick={onPing}>
               Ping admin
           </button>
       </div>
-  )
+    )
 }
 
 export default App
